1680fca3c85db66b03d5a05ae8431e5173449dd7
kUNQIjIANG <ucabkj0@ucl.ac.uk>
2017-11-12 12:15:56 +0000

    Update AE_keras.py

--- DeepLearning/AE_keras.py
+++ DeepLearning/AE_keras.py
@@ -2,10 +2,12 @@ from keras.layers import Input, Dense
 from keras.models import Model 
 from keras.datasets import mnist
 import numpy as np 
+
 # data setup
 (x_train,_), (x_test,_) = mnist.load_data()
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
+
 # from (60000,28,28）（10000,28,28) reshape into （60000,784）(10000,784)
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_train.shape[1:])))
