3c9639103827de7d8e5d4ec661cefbbb249993c6
Kunqi Jiang <ucabkj0@ucl.ac.uk>
2018-04-02 15:50:34 +0100

    Add files via upload

---
+++ Algorithm/TreeNode.py
@@ -0,0 +1,24 @@
+
+class TreeNode(object):
+    def __init__(self,left,right,value):
+        self.leftNode = left
+        self.rightNode = right
+        self.value = value
+
+
+    def setLeftNode(self,node):
+        self.leftNode = node
+
+    def setRightNode(self,node):
+        self.rightNode = node
+
+    def setValue(self,val):
+        self.value = val
+
+
+
+
+
+
+
+
