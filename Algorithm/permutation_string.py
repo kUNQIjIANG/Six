b0d568a3967e787d93657069c27eb61c49c86062
Kunqi Jiang <ucabkj0@ucl.ac.uk>
2018-06-23 22:27:07 +0100

    permutation of string with repetitive chars

--- Algorithm/permutation_string.py
+++ Algorithm/permutation_string.py
@@ -1,3 +1,11 @@
+链接：https://www.nowcoder.com/questionTerminal/fe6b651b66ae47d7acce78ffdd9a96c7?toCommentId=1451709
+来源：牛客网
+
+输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。 
+输入描述:
+输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母
+
+
 ## total number of permutation of string with repitition 
 ## swap first char with chars later which is not same,
 ## every swap add all permutation starting from the second char
